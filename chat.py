ee926eafeb332c96d009823dd4280e12ca4ae942
sabh2002 <sabh20020509@gmail.com>
2023-02-11 21:52:36 -0400

    Creacion de imagenes

--- chat.py
+++ chat.py
@@ -2,15 +2,45 @@ import openai
 
 openai.api_key = "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"
 
+def preguntas():
+    while True:
+
+        pregunta=input("\nPreguntame lo que quieras: ")
+
+        if pregunta=="salir":
+            break
+
+        respuesta=openai.Completion.create(engine="text-davinci-003",
+                                            prompt=pregunta,
+                                            max_tokens=2048,
+                                            temperature=0)
+        return respuesta.choices[0].text
+
+def imagenes():
+    while True:
+
+        imagen=input("\nQue imagen quieres crear: ")
+
+        if imagen=="salir":
+            break
+
+        respuesta=openai.Image.create(prompt=imagen,
+                                        n=2,
+                                        size="1024x1024")
+        print(respuesta.data[0].url)
+
 while True:
+    print("""
+    1)Pregunta
+    2)Imagen
+    3)Salir""")
+    op=int(input(">"))
 
-    pregunta=input("\nPreguntame lo que quieras: ")
-
-    if pregunta=="salir":
+    if op==1:
+        preguntas()
+    
+    elif op==2:
+        imagenes()
+    
+    elif op==3:
         break
-
-    respuesta=openai.Completion.create(engine="text-davinci-003",
-                                        prompt=pregunta,
-                                        max_tokens=2048,
-                                        temperature=0)
-    print(respuesta.choices[0].text)
